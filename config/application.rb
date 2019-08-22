6a2f4a47cf78a82ce41cc054e6cfc8d76a9d0faa
Jennifer <jenniferkjerstad@gmail.com>
2019-08-22 15:24:00 +0000

    8.22-1130

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
-
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 require_relative 'boot'
 
 require 'rails/all'
 
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
+
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 module Nomster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

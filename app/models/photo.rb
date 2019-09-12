ae19f73086ec1a738d09fac914ad57b6d92fb842
Jennifer <jenniferkjerstad@gmail.com>
2019-09-12 21:37:37 +0000

    9.12-1730

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :place
-  belongs_to :user
   validates :caption, presence: true
   mount_uploader :picture, PictureUploader
 end
--- db/migrate/20190822114509_create_add_picture_to_photos.rb
+++
@@ -1,8 +0,0 @@
-class CreateAddPictureToPhotos < ActiveRecord::Migration[5.2]
-  def change
-    create_table :add_picture_to_photos do |t|
-    	add_column :photos, :Picture, :string
-      t.timestamps
-    end
-  end
-end
